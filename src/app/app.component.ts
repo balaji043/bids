983b3b5c8aa7ee9793888f32e6b1bf812454cc33
Balaji Ramasamy <cubanhog.royalgamma@gmail.com>
2020-04-29 20:36:09 +0530

    final commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'bids';
+  BidsConstants = BidsConstants;
+  dropDownValues: string[] = [BidsConstants.HARDWARE, BidsConstants.SOFTWARE];
+
+  bidsForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.bidsForm = this.intializeBidsFormGroup();
+  }
+
+
+  public onSubmit(): void {
+    if (this.bidsType.invalid) {
+      return;
+    }
+    switch (this.bidsType.value) {
+      case BidsConstants.HARDWARE: {
+        if (this.hardWare.valid) {
+          console.log(this.hardWare.value);
+        }
+        break;
+      }
+      case BidsConstants.SOFTWARE: {
+        if (this.software.valid) {
+          console.log(this.software.value);
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+
+  public intializeBidsFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      bidsType: ['--Select--', Validators.required],
+      hardWare: this.formBuilder.group({
+        name: [null, Validators.required],
+        countryName: [null, Validators.required],
+        processor: [null, Validators.required]
+      }),
+      software: this.formBuilder.group({
+        name: [null, Validators.required],
+        countryName: [null, Validators.required],
+        version: [null, Validators.required]
+      }),
+    });
+  }
+
+
+  get bidsType() { return this.bidsForm.get('bidsType'); }
+  get hardWare() { return this.bidsForm.get('hardWare'); }
+  get software() { return this.bidsForm.get('software'); }
+
+}
+
+export class BidsConstants {
+  public static HARDWARE = 'Hardware';
+  public static SOFTWARE = 'Software';
 }
